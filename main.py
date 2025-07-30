db8e0a3095acb17787e75b4ade3db88c407ea534
theo2042 <theo2042@gmail.com>
2025-07-30 13:34:13 +0900

    bookbot complete

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from stats import num_words
 from stats import book_string
 from stats import sort_chars
+import sys
+
+
 
 def get_book_text(filepath):
     #takes a filepath as input and returns the contents of the file as a string.
@@ -13,21 +16,31 @@ def get_book_text(filepath):
 
 
 def main():
-    book_contents = get_book_text("books/frankenstein.txt")
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    count_words_text = num_words(book_contents)
-    print("----------- Word Count ----------")
-    print(f"Found {count_words_text} total words")
-    print("--------- Character Count -------")
-    converted_to_strings = book_string(book_contents)
+
     
-    sorted_chars = sort_chars(converted_to_strings)
-    sorted_alpha = []
-    for item in sorted_chars:
-        if item["char"].isalpha():
-            print(f"{item['char']}: {item['num']}")
-    print("============= END ===============")
+    if len(sys.argv) != 2:       
+           
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+
+    else:
+        book_location = sys.argv[1]       
+        book_contents = get_book_text(sys.argv[1])
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        count_words_text = num_words(book_contents)
+        print("----------- Word Count ----------")
+        print(f"Found {count_words_text} total words")
+        print("--------- Character Count -------")
+        converted_to_strings = book_string(book_contents)
+    
+        sorted_chars = sort_chars(converted_to_strings)
+        sorted_alpha = []
+        for item in sorted_chars:
+            if item["char"].isalpha():
+                print(f"{item['char']}: {item['num']}")
+        print("============= END ===============")
 
 
 main()
